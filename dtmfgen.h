752db65b022c0b6c8661624c5429c671cc343d63
dilshan <jayakody2000lk@gmail.com>
2019-05-09 08:42:19 +0530

    Add initial version of build automation script.

--- dtmfgen.h
+++ dtmfgen.h
@@ -1,7 +1,5 @@
 /* Copyright 2019 Dilshan R Jayakody.
  *
- * Copyright (c) 2019 Dilshan R Jayakody.
- *
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the "Software"), 
  * to deal in the Software without restriction, including without limitation 
